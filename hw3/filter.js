4c0189f92dfc7a5558bdae6d2a4a302baecbe0a8
Irina Safonova <irinasafonova2001@mail.ru>
2022-06-07 18:39:36 +0300

    hw3

--- hw3/filter.js
+++ hw3/filter.js
@@ -1,24 +1,27 @@
 const fs = require("fs");
 let numbers = [];
-let out = '';
-let out2 = '';
+let out = [];
+let out2 = [];
+let str1, str2;
 fs.readFile("data.txt", "utf8",
     function(error,data){
         numbers = data.split(' ');
         for (let elem of numbers){
             if (elem % 2 === 0){
-                out += elem + ' ';
+                out.push(elem);
             }
-            out2 += Math.pow(elem, 3) + ' ';
+            out2.push(Math.pow(elem, 3));
 
         }
-        fs.writeFile("out-1.txt", out, 'utf8', (err) => {
+        str1 = out.join(' ');
+        str2 = out2.join(' ');
+        fs.writeFile("out-1.txt", str1, 'utf8', (err) => {
                 if (err) {
                     console.error(err);
                 }
             }
         )
-        fs.writeFile("out-2.txt", out2, 'utf8', (err) => {
+        fs.writeFile("out-2.txt", str2, 'utf8', (err) => {
                 if (err) {
                     console.error(err);
                 }
